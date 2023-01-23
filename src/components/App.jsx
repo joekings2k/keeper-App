d6cb53767728c56761e06cc6d944610ee2d6ad96
jokings2k <eromoselejonthan@gmail.com>
2023-01-23 15:23:52 +0100

    added map function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,27 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./note";
+import notes from "../notes";
+
+
+const noteStyle ={
+  display:"flex",
+  justifyContent:"space-evenly",
+  margin:"10px",
+  flexWrap: "wrap",
+}
+
+
 function App() {
   return (
     <div>
       <Header />
-      <Note/>
+      <div style={noteStyle}>
+        {notes.map((note, i) => {
+          return <Note key={i} title={note.title} content={note.content} />;
+        })}
+      </div>
+
       <Footer />
     </div>
   );
--- src/components/note.jsx
+++ src/components/note.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "../styles/notes.css";
 
-function Note() {
+function Note(props) {
   return (
     <div className="card">
       <div className="innerc">
-        <h1 className="text">This is the note title</h1>
+        <h1 className="text">{props.title}</h1>
         <hr/>
-        <p className="text">This is the note content</p>
+        <p className="text">{props.content} </p>
       </div>
     </div>
   );
